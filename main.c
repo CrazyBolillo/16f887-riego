0576fc290c9d3b64c30c73ddaa23d3620b13e855
CrazyBolillo <40015596+CrazyBolillo@users.noreply.github.com>
2021-12-02 17:16:38 -0600

    Moved MOSFET driver pin to PORTE

--- main.c
+++ main.c
@@ -4,17 +4,21 @@
  *
  * Created on December 1, 2021, 5:31 PM
  */
-#pragma config DEBUG = 1
-#pragma config LVP = 0
-#pragma config FCMEN = 0
-#pragma config IESO = 0
-#pragma config BOREN = 00
-#pragma config CPD = 0
-#pragma config CP = 0
-#pragma config MCLRE = 0
-#pragma config PWRTE = 1
-#pragma config WDTE = 0
-#pragma config FOSC = 101
+// CONFIG1
+#pragma config FOSC = INTRC_CLKOUT// Oscillator Selection bits (INTOSC oscillator: CLKOUT function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = ON       // RE3/MCLR pin function select bit (RE3/MCLR pin function is MCLR)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown Out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
+
+// CONFIG2
+#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
 
 #include "config.h"
 #include "lcd.h"
@@ -67,7 +71,7 @@ uint8_t menu_buffer = 0;
 uint8_t pushed_button;
 uint8_t cursor;
 
-#define WATER_VALVE PORTCbits.RC1 = PORTCbits.RC2
+#define WATER_VALVE PORTEbits.RE2
 
 void menu_loop_bichar(unsigned char *menu, uint8_t eeprom_addr, char opt1, char opt2);
 void update_state(void);
@@ -92,10 +96,14 @@ void main(void) {
     WPUB = TRISB;
     
     // Setup analog ports for reading
-    ANSEL = 0xFF;
+    ANSEL = 0x0F;
     TRISA = 0xFF;
     ADCON0 = 0x81;
     ADCON1 = 0x00;
+    
+    // Port that will control water valve
+    PORTE = 0x00;
+    TRISE = 0x00;
     
     // Setup USART
     TXSTAbits.TXEN = 1;
@@ -267,7 +275,7 @@ void __interrupt() handle_interrupt() {
             uart_write(humid_str);
         }
         if (state == ST_ON) {
-            if ((humidity / 1000) >= trigger) {
+            if ((humidity) >= (trigger * 10)) {
                 WATER_VALVE = 0;
             }
             else {
